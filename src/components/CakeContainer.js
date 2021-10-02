8c7000909719d9b5a10f20effb02ff77233e234e
Indranil Halder <indranilhalder.dev@gmail.com>
2021-10-03 00:10:35 +0530

    React Redux Basics

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,25 @@
+import { buyCake } from "../redux";
+import { connect } from "react-redux";
+
+const CakeContainer = (props) => {
+  return (
+    <div>
+      <h1>Number of Cakes - {props.numberOfCakes}</h1>
+      <button onClick={props.buyCake}>Buy Cake</button>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    numberOfCakes: state.numberOfCakes,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyCake: () => dispatch(buyCake()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
